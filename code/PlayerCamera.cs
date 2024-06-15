14a397cf0d252d3df79befb0a07ee962dc063784
Kira <kira.eva@protonmail.com>
2024-06-15 12:07:32 +0100

    styling and refactors
    - mainly styling on navslot and the windows they spawn
    - rename inventory scene
    - move window code to window folder

--- code/PlayerCamera.cs
+++ code/PlayerCamera.cs
@@ -2,7 +2,7 @@
 
 public sealed class PlayerCamera : Component
 {
-    /*
+	/*
          Camera Config
         "Position": "693.1677,0.00001371071,278.5649",
         "Rotation": "0.180057,-0.000000007870536,-0.9836563,-0.00000004299698"
